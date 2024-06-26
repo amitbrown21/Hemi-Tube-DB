355117a08445b3e985b72bf38ca312203c787f8e
amitbrown21 <amitbrown21@gmail.com>
2024-06-26 16:15:35 +0300

    mongodb
    download

--- Server/app.js
+++ Server/app.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
