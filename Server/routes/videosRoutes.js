ac9d5dfd18a1eb36a8cd1375a6ec76608cab4e37
ShonTrubin <46036966+ShonTrubin@users.noreply.github.com>
2024-07-01 18:33:27 +0300

    fixed the likes and dislikes

--- Server/routes/videosRoutes.js
+++ Server/routes/videosRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const videosController = require("../controllers/videosController");
 
-
 router.get("/", videosController.getAllVideosWithTopAndRandom);
 router.post("/:pid/incrementViews", videosController.incrementViews);
 router.post("/:pid/incrementLikes", videosController.incrementLikes);
+router.post("/:pid/decrementLikes", videosController.decrementLikes);
 router.post("/:pid/incrementDislikes", videosController.incrementDislikes);
+router.post("/:pid/decrementDislikes", videosController.decrementDislikes);
 router.get("/all", videosController.getAllVideos);
+
 module.exports = router;
